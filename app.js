356a072deae0136f0a2f4a40261d55e0b4073429
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2022-01-05 21:46:03 +0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const indexfile = path.join(__dirname, "./", "views", "index.html");
+// const users = require('./data').userDB;
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// // app.use(express.static(path.join(__dirname,'./public')));
+
+// app.get('/', (req, res) => {
+//     res.send(indexfile)
+// })
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const indexfile = path.join(__dirname, "./", "views", "index.html");
-const users = require('./data').userDB;
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
-// app.use(express.static(path.join(__dirname,'./public')));
 
-app.get('/', (req, res) => {
-    res.send(indexfile)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    console.log(req.url);
+    next()
+});
+app.use("/")
+
+app.get("/index.html", (req, res) => {
+    res.sendFile(path.join(__dirname, './', 'views', 'index.html'))
 })
+
+app.post("/home", (req, res) => {
+    res.send(JSON.stringify("welcome " + req.body.userName + " Your password is " + req.body.password))
+})
+
+
+app.listen(3000);
+
+
+// https://codex.so/handling-any-post-data-in-express
